307d5c1f13c9df940b54a99ba52abaa7a23c17ca
Rajesh Upadhyay <rajesh.upadhyay@publicissapient.com>
2022-05-19 12:01:41 +0530

    xcv

--- instagramboot/src/main/java/com/instagramboot/InstagrambootApplication.java
+++ instagramboot/src/main/java/com/instagramboot/InstagrambootApplication.java
@@ -2,7 +2,7 @@ package com.instagramboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class InstagrambootApplication {
 
